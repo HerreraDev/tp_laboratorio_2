1990c2b9ce5c49f0bdc64b742e6c4c71604de9f1
martin herrera <martinherrera5000@hotmail.com>
2020-11-01 23:58:20 -0300

    Terminado

---
+++ Herrera.Martin.2D.TP3/Unit Testing/Excepciones.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Clases_Abstractas;
+using Clases_Instanciables;
+using Excepciones;
+
+namespace Unit_Testing
+{
+    [TestClass]
+    public class Excepciones
+    {
+        /// <summary>
+        /// Testea que se lance la excepcion DniInvalidoException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(DniInvalidoException))]
+        public void TestDniInvalidoException()
+        {
+            Profesor dniInvalido = new Profesor(01,"Manolo","Lamas","1p23p4",Persona.ENacionalidad.Argentino);
+        }
+
+        /// <summary>
+        /// Testea que se lance la excepcion NacionalidadInvalidaException
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(NacionalidadInvalidaException))]
+        public void TestNacionalidadInvalidaException()
+        {
+            Profesor dniInvalido = new Profesor(01, "Manolo", "Lamas", "87656788", Persona.ENacionalidad.Extranjero);
+        }
+
+        /// <summary>
+        /// Testea que no sea null la lista de alumons que se crea por cada jornada
+        /// </summary>
+        [TestMethod]
+        public void Test_Coleccion()
+        {
+            Profesor testProfesor = new Profesor();
+            Jornada testJornada = new Jornada(Universidad.EClases.Laboratorio,testProfesor);
+
+            Assert.IsNotNull(testJornada.Alumnos);
+
+        }
+    }
+}
